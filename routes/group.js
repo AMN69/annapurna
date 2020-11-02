616a149883931dd7a9845a063b35c39c3de63195
Daniel <danieljds@gmail.com>
2020-11-02 16:07:59 +0100

    models fix

--- routes/group.js
+++ routes/group.js
@@ -10,60 +10,75 @@ const withAuth = require("../helpers/middleware");
 
 // Group model
 const Group = require("../models/group");
+const People = require("../models/people");
+
+//GET Group create (admin)
+router.get("/grcreadm", withAuth, async (req, res, next) => {
+  res.render("auth/grcreadm");
+});
+
+//POST Group create (admin)
+
+router.post("/grcreadm", withAuth, async (req, res, next) => {
+  const { adminName, adminSurname, groupName, groupDescription } = req.body;
+  //console.log("este es el user id", req.user._id);
+
+  try {
+    const newGroup = await Group.create({
+      idAdmin: req.user._id,
+      adminName,
+      adminSurname,
+      groupName,
+      groupDescription,
+    });
+    console.log(newGroup);
+    res.redirect("/grcreadm");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/grlistadm");
+  }
+});
 
 //GET Group List (Admin)
-console.log("antes del router")
-router.get("/grlistadm", (req, res, next) => {
-    console.log("antes del render")
-    res.render("auth/grlistadm")
+
+router.get("/grlistadm", withAuth, async (req, res, next) => {
+  if (req.user) {
+    const user = req.user;
+    const userId = req.user._id;
+    console.log(userId, "ENCONTRAMOS EL USER ID");
+    try {
+      const groupListAdmin = await Group.find();
+      console.log(groupListAdmin, "GROUP LIST!!!!");
+      // To take all groupList and show first the user ones (Group.idPeople[] = user)
+      res.render("auth/grlistadm", groupListAdmin);
+    } catch (error) {
+      next(err);
+      return;
+    }
+  } else {
+    res.redirect("/");
+  }
 });
 
-router.post("/grlistadm", async (req, res, next) => {
-    const { groupName, groupDescription, idPeople, idMeetup } = req.body;
-
-    await Group.create({
-        adminName,
-        adminSurname,
-        groupName,
-        groupDescription,
-        idPeople,
-        idMeetup,
-    });
-});
-
-//GET Group detail (admin) 
+//GET Group detail modify (admin)
 router.get("/grdetadm", (req, res, next) => {
-    res.render("auth/grdetadm");
+  res.render("auth/grdetadm");
 });
 
 //POST Group detail (admin /grdetadm
 
-router.post('/grdetadm', function(req, res, next) {
-  const theNewGroup = new Group ({
-    adminName: req.body.group.adminName,
-    adminSurname: req.body.group.adminSurname,
-    groupName: req.body.group.groupName,
-    groupDescription: req.body.group.groupDescription,
+router.post("/grdetadm", function (req, res, next) {
+  const theNewGroup = new Group({
+    adminName: req.body.adminName,
+    adminSurname: req.body.adminSurname,
+    groupName: req.body.groupName,
+    groupDescription: req.body.groupDescription,
   });
 
-
-  theNewGroup.save ((err) => {
-    if (err) {
-      res.render('/grdetadm', {
-        title: "Create a New Group"
-      });
-    }
-    else {
-      res.redirect('/grlistus');
-    }
-  });
-});
-
-
-//GET Group list /grlistus
-router.get("/grlistus", withAuth, async (req, res, next) => {
-  if (req.user) {
-    // const user = req.user;
+  //GET Group list /grlistus
+  router.get("/grlistus", withAuth, async (req, res, next) => {
+    if (req.user) {
+      // const user = req.user;
       try {
         const groupList = await Group.find();
         // To take all groupList and show first the user ones (Group.idPeople[] = user)
@@ -72,32 +87,31 @@ router.get("/grlistus", withAuth, async (req, res, next) => {
         next(err);
         return;
       }
-  } else {
-    res.redirect("/");
-  }      
-});
-  
-// POST Update Group list /grlistus NO ME QUEDA CLARO COMO HACERLO. TENGO COPIADO EL DE ADMIN
-  
-router.post('/grlistus', function(req, res, next) {
-  const theNewGroup = new Group ({
-    adminName: req.body.group.adminName,
-    adminSurname: req.body.group.adminSurname,
-    groupName: req.body.group.groupName,
-    groupDescription: req.body.group.groupDescription,
+    } else {
+      res.redirect("/");
+    }
   });
 
+  // POST Update Group list /grlistus NO ME QUEDA CLARO COMO HACERLO. TENGO COPIADO EL DE ADMIN
 
-  theNewGroup.save ((err) => {
-    if (err) {
-      res.render('/grdetadm', {
-        title: "Create a New Group"
-      });
-    }
-    else {
-      res.redirect('/grlistus');
-    }
-  })
+  router.post("/grlistus", function (req, res, next) {
+    const theNewGroup = new Group({
+      adminName: req.body.group.adminName,
+      adminSurname: req.body.group.adminSurname,
+      groupName: req.body.group.groupName,
+      groupDescription: req.body.group.groupDescription,
+    });
+
+    theNewGroup.save((err) => {
+      if (err) {
+        res.render("/grdetadm", {
+          title: "Create a New Group",
+        });
+      } else {
+        res.redirect("/grlistus");
+      }
+    });
+  });
 });
 
 module.exports = router;
--- routes/meetup.js
+++ routes/meetup.js
@@ -8,6 +8,75 @@ const withAuth = require("../helpers/middleware");
 // Meetup model
 const Meetup = require("../models/meetup");
 
+
+////GET MEETUP CREATE (admin)
+router.get("/mecreadm", withAuth, async (req, res, next) => {
+  res.render("auth/mecreadm");
+});
+
+//POST Meetup create (admin)
+
+router.post('/mecreadm', function(req, res, next) {
+
+  const { name, description, date, time, country, city, zipcode, adress, adressnum, mapPoint } = req.body; 
+  
+  const theNewMeetup = new Meetup ({
+    name,
+    description,
+    date,
+    time,
+    country,
+    city,
+    zipcode,
+    adress,
+    adressnum,
+    mapPoint 
+  })
+
+
+  theNewMeetup.save ((err) => {
+    if (err) {
+      console.log(err)
+      res.render('auth/mecreadm', {
+        title: "Try again"
+      });
+    }
+    else {
+      res.redirect('/melistadm');
+    }
+  })
+});
+//GET MEETUP LIST ADMIN
+
+router.get("/melistadm", withAuth, async (req, res, next) => {
+  if (req.user) {
+    const user = req.user;
+      try {
+        const meetupListAdmin = await Meetup.find();
+        // To take all meetuplist and show the ones the groups have (Group.idPeople[] = user)
+        res.render("auth/melistadm", meetupListAdmin);
+      } catch (error) {
+        next(err);
+        return;
+      }
+  } else {
+    res.redirect("/");
+  }      
+});
+
+//GET Meetup detail (admin) /medetadm
+router.get("/medetadm:id", async (req, res, next) => {
+  const idMeetup = req.params.id;
+  try {
+    let meetupList = await Meetup.find([{idGroup: idGroup}]);
+    res.render("auth/melistadm", meetupList);
+  } catch (error) {
+    next(err);
+    return;
+  }
+});
+
+
 // GET Meetup list /melistus
 router.get("/melistus:id", async (req, res, next) => {
   const idGroup = req.params.id;
@@ -65,29 +134,6 @@ router.get("/medetadm:id", (req, res, next) => {
   res.render("auth/medetadm");
 });
 
-//POST Meetup detail (admin)
-// I think it's not necessary but think about it.
 
-router.post('/medetadm', function(req, res, next) {
-  const theNewMeetup = new Meetup ({
-      name: req.body.meetup.name,
-      description: req.body.meetup.description,
-      date: req.body.meetup.date,
-      time: req.body.meetup.time
-      //coordenadas
-  });
 
-  
-  theNewMeetup.save ((err) => {
-    if (err) {
-      res.render('melistus', {
-        title: "Create a New Meet Up"
-      });
-    }
-    else {
-      res.redirect('/melistus');
-    }
-  })
-});
- 
 module.exports = router;
