83ffa165e1f921dc1d8c70bfa3efa092eb6cc538
Andreu Martínez <martinez.andreu@gmail.com>
2020-11-04 09:53:38 +0100

    [AMN] Fixes Dani/Leandro 20201104 9:53

--- routes/meetup.js
+++ routes/meetup.js
@@ -7,6 +7,8 @@ const withAuth = require("../helpers/middleware");
 
 // Meetup model
 const Meetup = require("../models/meetup");
+// Group model
+const Group = require("../models/group");
 
 
 ////GET MEETUP CREATE (admin)
@@ -101,14 +103,18 @@ router.get("/melistus/:id", withAuth, async (req, res, next) => {
 
 
 //GET Meetup detail (user) /medetus
-router.get("/medetus:id", async (req, res, next) => {
+// console.log("Justo antes de metdetus:id");
+router.get("/medetus/:id", async (req, res, next) => {
+  // console.log("Entra en metdeus");
   const idMeetup = req.params.id;
   try {
-    let meetupDet = await Meetup.find([{_id: idMeetup}]);
+    let meetupDet = await Meetup.findById(idMeetup);
+    console.log("meetupDet: ", meetupDet);
     const api = "https://image.maps.ls.hereapi.com/mia/1.6/mapview?apiKey=" + process.env.API_KEY + "&co=" + meetupDet.country + "&ci=" + meetupDet.city + "&zi=" + meetupDet.zipcode + "&s=" + meetupDet.address + "&n=" + meetupDet.addressnum + "&z=17&h=320&f=1";
     res.render("auth/medetus", {meetupDet, api});
   } catch (error) {
-    
+    next(error);
+    return;
   }
 });
 
